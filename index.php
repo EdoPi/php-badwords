ea8252c111c1f81a4fb8cdc55cf74937d9be48fc
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-02-25 17:36:34 +0100

    un pò di css

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $newText= str_replace($_GET['replace'],' *** ', $text);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Test PHP</title>
 </head>
 <body>
@@ -25,14 +26,14 @@ $newText= str_replace($_GET['replace'],' *** ', $text);
     </header>
     <div class="main">
         <h1>Lunghezza Paragrafo</h1>
-        <p>la lungezza del mio testo è <?php echo $textLenght ?> caratteri</p>
+        <p class="length">la lungezza del mio testo è <span><?php echo $textLenght ?></span> caratteri</p>
 
         <h2>My text</h2>
         
-        <p><b>Il mio testo è: </b><?php echo $text?></p>
+        <p class="text"><b>Il mio testo è: </b><?php echo $text?></p>
 
         <h2>My Text Modified</h2>
-        <p><b>Il nuoco testo con la parola modificata è: </b><?php echo $newText ?> </p>
+        <p class="new-text"><b>Il nuoco testo con la parola modificata è: </b><?php echo $newText ?> </p>
 
     </div>
     
